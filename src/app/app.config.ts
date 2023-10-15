c7552389ed635c7479965994b57ab6f1a63d82a3
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-15 17:15:08 +0200

    refactor: router input bindings, load flight on init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { NextFlightsModule } from './next-flights/next-flights.module';
 import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideRouter(APP_ROUTES),
+    provideRouter(APP_ROUTES,
+      withComponentInputBinding()
+    ),
     importProvidersFrom(NextFlightsModule),
     importProvidersFrom(MatDialogModule)
   ]
--- src/app/flight-booking/features/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/features/flight-edit/flight-edit.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, Input, OnInit, booleanAttribute, inject, numberAttribute } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
-import { distinctUntilChanged, map, switchMap, tap } from 'rxjs';
+import { distinctUntilChanged, finalize, map, switchMap, tap } from 'rxjs';
 import { initFlight } from '../../../model/flight';
 import { ValidationErrorsComponent } from '../../../shared/validation/validation-errors/validation-errors.component';
 import { FlightService } from '../../data-access/flight.service';
@@ -15,34 +15,17 @@ import { FlightService } from '../../data-access/flight.service';
   templateUrl: './flight-edit.component.html',
   styleUrls: ['./flight-edit.component.css'],
 })
-export class FlightEditComponent {
+export class FlightEditComponent implements OnInit {
   private flightService = inject(FlightService);
   private route = inject(ActivatedRoute);
 
-  id = 0;
-  showDetails = false;
+  @Input({ transform: numberAttribute }) id = 0;
+  @Input({ transform: booleanAttribute }) showDetails = false;
+
   flight = initFlight;
 
-  constructor() {
-    this.route.paramMap.pipe(
-      // Create local state w/ correct typing
-      map(params => ({
-        id: +(params.get('id') || 0),
-        showDetails: params.get('showDetails') === 'true'
-      })),
-      // Set class props based on state
-      tap(({id, showDetails}) => {
-        this.id = id;
-        this.showDetails = showDetails;
-      }),
-      // Map to ID
-      map(({id}) => id),
-      // Filter same IDs
-      distinctUntilChanged(),
-      // Load Flights based on ID
-      switchMap(id => this.flightService.findById(this.id))
-    ).subscribe(
-      // Set flights class prop
+  ngOnInit(): void {
+    this.flightService.findById(this.id).subscribe(
       flight => this.flight = flight
     );
   }
